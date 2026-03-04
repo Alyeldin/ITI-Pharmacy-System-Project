83ed71edb61434c47608e91fc0d702536aee9ac2
Aly alkolaly <alieldin1908944@miuegypt.edu.eg>
2026-03-04 20:04:14 +0200

    Side bar & routing
    -Created a side bar that have all view pages included
    -Created the routing part in app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when("/adminDashboard", {
+      templateUrl: "views/adminDashboard.html",
+      controller: "",
+    })
+    .when("/checkout", {
+      templateUrl: "views/checkout.html",
+      controller: "",
+    })
+    .when("/profilePage", {
+      templateUrl: "views/profilePage.html",
+      controller: "",
+    })
+    .when("/returnProduct", {
+      templateUrl: "views/returnProduct.html",
+      controller: "",
+    })
+    .when("/salesHistory", {
+      templateUrl: "views/salesHistory.html",
+      controller: "",
+    })
+    .when("/showCustomers", {
+      templateUrl: "views/showCustomers.html",
+      controller: "",
+    })
+    .when("/showInvoices", {
+      templateUrl: "views/showInvoices.html",
+      controller: "",
+    })
+    .when("/showProducts", {
+      templateUrl: "views/showProducts.html",
+      controller: "",
+    })
+    .when("/showShifts", {
+      templateUrl: "views/showShifts.html",
+      controller: "",
+    })
+    .when("/showUsers", {
+      templateUrl: "views/showUsers.html",
+      controller: "",
+    })
+    .otherwise({
+      redirectTo: "/",
+    });
+});
